c460c059dd9e5fc876167bdc9e3dbfb715bca7b0
Roger Dubar <rdubar@gmail.com>
2023-02-21 23:33:51 +0000

    Added randonmcase and more

--- venv/routes/index.py
+++ venv/routes/index.py
@@ -1,20 +1,33 @@
-from flask import render_template
+from flask import render_template, request
 from . import routes
 
 import secrets
 import string
 
+
 def generate_password(length=20, safe=False):
     if safe:
-        return secrets.token_urlsafe(length)
+        password =''
+        while (not '_' in password) and (not '-' in password):
+            password = secrets.token_urlsafe(length)
+        return password
     else:
         alphabet = string.ascii_letters + string.digits + string.punctuation
         password = ''.join(secrets.choice(alphabet) for i in range(length))
         return password
 
 def random_case(text):
-    return ''.join(choice((str.upper, str.lower))(c) for c in text)
+    return ''.join(secrets.choice((str.upper, str.lower))(c) for c in text)
 
 @routes.route('/')
 def index():
     return render_template('index.html', password=generate_password(safe=True))
+
+@routes.route('/randomcase/', methods=('GET', 'POST'))
+def randomcase():
+    text = ''
+    if request.method == 'POST':
+        text = request.form['text']
+    if text == '':
+        text = 'random text'
+    return render_template('former.html', text = random_case(text), title=random_case('randomcase'))
